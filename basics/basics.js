6a33536af477da6f682d8333a4585baccc0c6073
Charlize de Beer <chardebeer94@gmail.com>
2021-12-07 14:00:37 +0200

    [basics] Adding Basic Data Types

--- basics/basics.js
+++ basics/basics.js
@@ -9,3 +9,27 @@ function greeter(name){
 
 greeter("Charlize")
 
+//Adding Basic Data Types
+
+function doTheMath(secondInt, secondDec, secondStr){
+    const firstInt = 44;
+    const firstDec = 9.9;
+    const firstStr = "Kawaii ✨";
+
+    let int = parseInt(firstInt + secondInt);
+    console.log(int);
+    document.getElementById("int").innerHTML = firstInt + " + " + secondInt + " = " + int  ;
+
+
+    let dec = parseFloat(firstDec + secondDec);
+    console.log(dec);
+    document.getElementById("dec").innerHTML = firstDec + " + " + secondDec + " = " + dec  ;
+
+    let str = firstStr.concat(secondStr);
+    console.log(str);
+    document.getElementById("str").innerHTML = str ;
+
+}
+
+doTheMath(99, 4.4, ' Chaos')
+
